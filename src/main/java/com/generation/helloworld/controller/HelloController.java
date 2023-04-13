8574f15ced324e78a20e6bf6a5d9e84ea530b9d6
Letícia Queiroga <letqueirogaa@outlook.com>
2023-04-12 21:38:54 -0300

    [Feature Leticia - Task : Atividade Spring| 12.04.2023]

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -10,11 +10,33 @@ public class HelloController {
 	
 	@GetMapping
 	public String hello() {
-		return "<b>Hello World!</b>";
+		return "<big><b>Hello World!</b></big>";
 	}
 	
-	@GetMapping("/nome")
-	public String meuNome() {
-		return "<b>Letícia</b>";
+	@GetMapping("/bsm")
+	public String bsms() {
+		return "<big><b>BSM's</b></br></big>"
+				+ "Mentalidade de Crescimento</br>"
+				+ "Persistência</br>"
+				+ "Responsabilidade Pessoal</br>"
+				+ "Orientação ao Futuro</br>"
+				+ "Comunicação</br>"
+				+ "Orientação ao Detalhe</br>"
+				+ "Proatividade</br>"
+				+ "Trabalho em Equipe</br>"
+				+ "Gestão de Tempo</br>";
 	}
+	
+	@GetMapping("/objetivo")
+	public String objetivos() {
+		return "<big><b>Objetivos de Aprendizagem</b></br></big>"
+				+ "<b>Semana 6</b></br>"
+				+ "1. Aprender Spring</br>"
+				+ "2. Estudar mais sobre Bancos de Dados</br>"
+				+ "3. Melhorar minha gestão de tempo</br>"
+				+ "4. Tentar acompanhar o evento SprintAI900</br>"
+				+ "5. Exercitar mais a comunicação</br>"
+				+ "6. Exercitar mais a proatividade</br>";
+	}
+	
 }
